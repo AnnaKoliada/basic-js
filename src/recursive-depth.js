e6a0c8ed8147ed5dae0a7e041dd8f0a059a88522
AnnaKoliada <anacirec@gmail.com>
2021-03-29 18:33:28 +0300

    add tests

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    
+      let maxim = 1;
+      let a = 1;
+  
+      if (Array.isArray(arr)) {
+        for (let elem of arr) {
+          if (Array.isArray(elem)) {
+            a += this.calculateDepth(elem);
+          }
+          if (a > maxim) {
+            maxim = a;
+          }
+          a = 1;
+        }
+      }
+  
+      return maxim;
+    
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(boolen) {
+    this.directMachine = true;
+    this.reverseMachine = false;
+    this.arr = [];
+  }
+  tabulaRecta = {
+    a: "abcdefghijklmnopqrstuvwxyz",
+    b: "bcdefghijklmnopqrstuvwxyza",
+    c: "cdefghijklmnopqrstuvwxyzab",
+    d: "defghijklmnopqrstuvwxyzabc",
+    e: "efghijklmnopqrstuvwxyzabcd",
+    f: "fghijklmnopqrstuvwxyzabcde",
+    g: "ghijklmnopqrstuvwxyzabcdef",
+    h: "hijklmnopqrstuvwxyzabcdefg",
+    i: "ijklmnopqrstuvwxyzabcdefgh",
+    j: "jklmnopqrstuvwxyzabcdefghi",
+    k: "klmnopqrstuvwxyzabcdefghij",
+    l: "lmnopqrstuvwxyzabcdefghijk",
+    m: "mnopqrstuvwxyzabcdefghijkl",
+    n: "nopqrstuvwxyzabcdefghijklm",
+    o: "opqrstuvwxyzabcdefghijklmn",
+    p: "pqrstuvwxyzabcdefghijklmno",
+    q: "qrstuvwxyzabcdefghijklmnop",
+    r: "rstuvwxyzabcdefghijklmnopq",
+    s: "stuvwxyzabcdefghijklmnopqr",
+    t: "tuvwxyzabcdefghijklmnopqrs",
+    u: "uvwxyzabcdefghijklmnopqrst",
+    v: "vwxyzabcdefghijklmnopqrstu",
+    w: "wxyzabcdefghijklmnopqrstuv",
+    x: "xyzabcdefghijklmnopqrstuvw",
+    y: "yzabcdefghijklmnopqrstuvwx",
+    z: "zabcdefghijklmnopqrstuvwxy"
+  }
+
+  encrypt(message, key) {
+    if(!message || !key) {
+      throw new Error('ERROR');
+    }
+
+    for (let i=message[1]; i <=message.length; i++){
+      message[i] = this.tabulaRecta[key[i]][message[i].charCodeAt() - 97];
+      return(this.arr.push(message[i]))
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if(!encryptedMessage || !key) {
+      throw new Error('ERROR');
+    }
   }
 }
 
